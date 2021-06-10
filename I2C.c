3f465e152c661eb6e6868c363bb129030d4a442a
Kenneth Henderick <kenneth@ketronic.be>
2021-06-10 06:07:22 +0200

    Added more comments

--- I2C.c
+++ I2C.c
@@ -6,17 +6,17 @@ unsigned char module_type = 0;
 
 void i2c_configure()
 {
-  SSPCON1 = 0b00101000;
+  SSPCON1 = 0b00101000;  // Set SSPEN and setting SSPM to I2C Master mode, expecting clock = FOSC/(4 * (SSPADD + 1))
   SSPCON2 = 0;
   SSPADD = (/* clock frequency */4000000/(4 * /* i2c frequency */ 100000))-1;
   SSPSTAT = 0;
-  TRISCbits.TRISC3 = 1;
+  TRISCbits.TRISC3 = 1;  // Set SDA/SCL direction
   TRISCbits.TRISC4 = 1;
 }
 
 void i2c_wait()
 {
-  while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F)); //Transmit is in progress
+  while ((SSPSTAT & 0x04 /* R/~W: transmit in progress */) || (SSPCON2 & 0x1F /* anything is busy */));
 }
 
 void i2c_start()
@@ -46,19 +46,19 @@ void i2c_write(unsigned char d)
 void init_i2c(void) {
     i2c_configure();
     
-    write_register(CHIP_0, 6, 0);
-    write_register(CHIP_0, 7, 0);
-    write_register(CHIP_1, 6, 0);
-    write_register(CHIP_1, 7, 0);
+    write_register(CHIP_0, DIRECTION_REGISTER_0, 0);
+    write_register(CHIP_0, DIRECTION_REGISTER_1, 0);
+    write_register(CHIP_1, DIRECTION_REGISTER_0, 0);
+    write_register(CHIP_1, DIRECTION_REGISTER_1, 0);
     
-    write_register(CHIP_0, 2, 0b11111111);
-    write_register(CHIP_0, 3, 0b11111111);
-    write_register(CHIP_1, 2, 0b11111111);
-    write_register(CHIP_1, 3, 0b11111111);
+    write_register(CHIP_0, OUTPUT_REGISTER_0, 0b11111111);
+    write_register(CHIP_0, OUTPUT_REGISTER_1, 0b11111111);
+    write_register(CHIP_1, OUTPUT_REGISTER_0, 0b11111111);
+    write_register(CHIP_1, OUTPUT_REGISTER_1, 0b11111111);
     
     unsigned8 address_byte = read_eeprom_byte(EEPROM_ADDR_ADDR);
     module_type = 0;  // Unknown
-    if (address_byte == 79) { // 79 == 'O'
+    if (address_byte == 79) { // 79 == ord('O')
         module_type = 1;  // OMH06 expected
     } else if (address_byte < 255) {
         module_type = 2;  // OMH03 expected
@@ -78,19 +78,19 @@ void write_register(unsigned8 address, unsigned8 reg, unsigned8 data) {
 void write_orange_status_led(boolean on) {
     if (module_type == 1) {
         if (on) {
-            write_register(CHIP_1, 3, 0b11001111);
-            //                            GR
+            write_register(CHIP_1, OUTPUT_REGISTER_1, 0b11001111);
+            //                            GR leds
         } else {
-            write_register(CHIP_1, 3, 0b11111111);
-            //                            GR
+            write_register(CHIP_1, OUTPUT_REGISTER_1, 0b11111111);
+            //                            GR leds
         }
     } else if (module_type == 2) {
         if (on) {
-            write_register(CHIP_1, 3, 0b11111001);
-            //                               GR
+            write_register(CHIP_1, OUTPUT_REGISTER_1, 0b11111001);
+            //                               GR leds
         } else {
-            write_register(CHIP_1, 3, 0b11111111);
-            //                               GR
+            write_register(CHIP_1, OUTPUT_REGISTER_1, 0b11111111);
+            //                               GR leds
         }
     }
 }
--- I2C.h
+++ I2C.h
@@ -5,6 +5,13 @@
  
     #define CHIP_0 0b11101100
     #define CHIP_1 0b11101010
+    //               |   |||+-> R/~W bit
+    //               |   |++-> Address
+    //               +   +-> Fixed part
+    #define DIRECTION_REGISTER_0 6
+    #define DIRECTION_REGISTER_1 7
+    #define OUTPUT_REGISTER_0 2
+    #define OUTPUT_REGISTER_1 3
     
     void init_i2c(void);
     void write_register(unsigned8 address, unsigned8 reg, unsigned8 data);
